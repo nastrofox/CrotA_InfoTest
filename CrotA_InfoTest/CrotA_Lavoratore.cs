72027c4907f613a0635b181202768958326dc67e
crotti.19315 <crotti.19315@LABI1-6PC28>
2023-03-27 09:33:08 +0200

    Task3

---
+++ CrotA_InfoTest/CrotA_Lavoratore.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrotA_InfoTest
+{
+    internal class CrotA_Lavoratore:CrotA_Candidato
+    {
+        public int CrotA_esperienze;
+        public int CrotA_Esperienze
+        {
+            get { return CrotA_esperienze; }
+            set { if (value >= 0 && value <= 5)
+                {
+                    CrotA_esperienze = value;
+                }
+                
+            }
+        }
+        public CrotA_Lavoratore():base()
+        {
+            CrotA_esperienze = 0;
+        }
+        public CrotA_Lavoratore(int crotA_esperienze, int crotA_Esperienze):base(matricola,nome)
+        {
+            CrotA_Esperienze = crotA_esperienze;
+        }
+        public override int punteggio()
+        {
+            return 20 * CrotA_esperienze;
+        }
+        public override bool isIdoneo()
+        {
+            if(punteggio()>=60)
+                return true;
+            else
+                return false;
+        }
+    }
+}
