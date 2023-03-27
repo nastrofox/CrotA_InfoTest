f55c1750c84897cf7825b594e59b20cc5a8cc525
crotti.19315 <crotti.19315@LABI1-6PC28>
2023-03-27 08:33:38 +0200

    Task1

---
+++ CrotA_InfoTest/CrotA_Candidato.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrotA_InfoTest
+{
+    internal class CrotA_Candidato
+    {
+        public int CrotA_matricola;
+        public string CrotA_nome;
+
+        public CrotA_Candidato(int crotA_matricola, string crotA_nome)
+        {
+            this.CrotA_matricola = crotA_matricola;
+            this.CrotA_nome = crotA_nome;
+        }   
+        public int crotA_matricola
+        {
+            set;get;
+        }
+        public int crotA_nome
+        {
+            set; get;
+        }
+        public CrotA_Candidato()
+        {
+
+        }
+        public CrotA_Candidato(int crotA_matricola)
+        {
+            this.CrotA_matricola = crotA_matricola;
+        }
+        public CrotA_Candidato(int crotA_matricola, string crotA_nome)
+        {
+            this.CrotA_matricola = crotA_matricola;
+            this.CrotA_nome = crotA_nome;
+        }
+        public abstract bool isIdoneo()
+        {
+            return false;
+        }
+        public abstract int punteggio()
+        {
+            return 0;
+        }
+    }
+}
